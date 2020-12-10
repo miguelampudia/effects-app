5f641425e1498d2524216e3441e45a4b026c5edd
Miguel Angel Ampudia Gomez <miguel.ampudia@outlook.es>
2020-12-10 02:44:24 -0500

    ini proyecto angular redux

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,12 @@
+export class Usuario {
+
+    constructor(
+        public id: number,
+        public email: string,
+        public firstname: string,
+        public lastname: string,
+        public avatar: string
+    ) {
+
+    }
+}
